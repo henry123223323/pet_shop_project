e7b7cd990231ad07b1ba5bd625445763ab3751f4
henry123223323 <henry123223323@gmail.com>
2025-04-22 09:55:04 +0800

    react初版

---
+++ fashion-paw/src/component/MemberCenter/MyAccount.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class MyAccount extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>這裡是我的帳號</h1>
+        );
+    }
+}
+ 
+export default MyAccount;
