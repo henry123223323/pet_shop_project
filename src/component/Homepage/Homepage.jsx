4f51b749041c8d1fe97e0f26d3dd458c44dd6cdd
gyiiyg <Angela222115@gmail.com>
2025-04-22 11:07:10 +0800

    新增css

---
+++ src/component/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Homepage extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>這是主頁</h1>
+        );
+    }
+}
+ 
+export default Homepage;
