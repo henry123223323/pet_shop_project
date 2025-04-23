771a241120a9a7f0f4b97f48f8f6695d6f1c107a
gyiiyg <Angela222115@gmail.com>
2025-04-23 11:47:06 +0800

    部位觸碰v1

---
+++ fashion-paw/src/component/PetKnowledge/PartTouch/Touch.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Touch extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>部位碰碰看</h1>
+        );
+    }
+}
+ 
+export default Touch;
