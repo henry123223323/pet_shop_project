6a572240d4a12a4a37db1f6fd130769a49034d96
gyiiyg <Angela222115@gmail.com>
2025-04-24 02:20:09 +0800

    未完成

--- fashion-paw/src/component/PetKnowledge/PartTouch/Touch.jsx
+++ fashion-paw/src/component/PetKnowledge/PartTouch/Touch.jsx
@@ -4,10 +4,10 @@ class Touch extends Component {
     render() {
         return (
             <div className="container-xl">
-                <h1 className='paw-bg-primary'>部位碰碰看</h1>
+                <h2 className='border rounded paw-bg-primary text-center'>部位碰碰看</h2>
                 <label htmlFor="next">貓貓</label>
                 <label htmlFor="next">狗狗</label>
-                <button id='next'>▶️</button>
+                <a id='next'>▶️</a>
                 <img src="" alt="" />
                 <p></p>
                 <p></p>
@@ -16,15 +16,15 @@ class Touch extends Component {
                 <div>
                     <p>狗狗耳朵容易藏汙納垢，要定期清潔耳朵</p>
                 </div>
-                <h6 className="text-xl font-semibold mt-10 mb-4">｜或許你適合...</h6>
+                <h6 className="mt-5 mb-4">｜或許你適合...</h6>
 
                 <div className="container mb-5">
                     <div className="row g-4">
                         {["點心罐", "純湯罐", "無膠肉罐"].map((name, index) => (
                             <div key={index} className=" col">
-                                <div className="border rounded-lg p-3 shadow-md mx-2">
+                                <div className="border rounded p-3 shadow-md mx-2">
                                     <div className="text-center mb-2">
-                                        <img src="" alt="測試 hamster" className="img-fluid" style={{ width: '100px', height: 'auto', objectFit: 'cover' }} />
+                                        <img src="" alt="商品圖片" className="img-fluid" style={{ width: '100px', height: 'auto', objectFit: 'cover' }} />
                                     </div>
                                     <p className="text-sm">商品名稱: {name}</p>
                                     <p className="text-sm">價格: 80元</p>
