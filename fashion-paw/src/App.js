262c358ef57ec4dd106de5ce30e0be8e015820f9
“jia-yiii” <chocotin1624@gmail.com>
2025-04-25 11:06:05 +0800

    劉-新增商品圖窗元件測試

--- fashion-paw/src/App.js
+++ fashion-paw/src/App.js
@@ -2,28 +2,30 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import MemberCenter from './component/MemberCenter/MyAccount'
-import Homepage from './component/Homepage/Homepage';
-import ProductPage from './component/ProductPage/ProductPage';
-import Login from './component/MemberCenter/Login';
-import Register from './component/MemberCenter/Register';
-import Touch from './component/PetKnowledge/PartTouch/Touch';
-import DogGuide from './component/PetKnowledge/Novicefeeding/DogGuide';
+// import MemberCenter from './component/MemberCenter/MyAccount'
+// import Homepage from './component/Homepage/Homepage';
+// import ProductPage from './component/ProductPage/ProductPage';
+// import Login from './component/MemberCenter/Login';
+// import Register from './component/MemberCenter/Register';
+// import Touch from './component/PetKnowledge/PartTouch/Touch';
+// import DogGuide from './component/PetKnowledge/Novicefeeding/DogGuide';
+import PdImageGallery from './component/ProductDetailPage/top/PdImageGallery';
 
 function App() {
   return (
     <BrowserRouter>
-    <h1 className='paw-bg-primary top-0'>Header</h1>
+    {/* <h1 className='paw-bg-primary top-0'>Header</h1> */}
       <Switch>
-        <Route path="/" component={ Homepage } exact/>
+        <Route path="/topimage" component={ PdImageGallery } exact/>
+        {/* <Route path="/" component={ Homepage } exact/>
         <Route path="/MemberCenter" component={ MemberCenter } exact/>
         <Route path="/ProductPage" component={ ProductPage } exact/>
         <Route path="/MemberCenter/Login" component={ Login } exact/>
         <Route path="/MemberCenter/Register" component={ Register } exact/>
         <Route path="/Novicefeeding/DogGuide" component={DogGuide} exact/>
-        <Route path="/PartTouch/Touch" component={Touch} exact/>
+        <Route path="/PartTouch/Touch" component={Touch} exact/> */}
       </Switch>
-      <h1 className='paw-bg-secondary'>Footer</h1>
+      {/* <h1 className='paw-bg-secondary'>Footer</h1> */}
     </BrowserRouter>
   );
 }
