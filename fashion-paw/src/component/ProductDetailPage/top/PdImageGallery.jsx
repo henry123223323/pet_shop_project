65e56578716a76198be91a7415662ff3ecf84db5
“jia-yiii” <chocotin1624@gmail.com>
2025-04-25 13:35:49 +0800

    劉-完成商品窗框架

--- fashion-paw/src/component/ProductDetailPage/top/PdImageGallery.jsx
+++ fashion-paw/src/component/ProductDetailPage/top/PdImageGallery.jsx
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './PdImageGallery.module.css';
-class PdImageGallery extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-            <h1>這是商品圖片窗</h1>
-            <div className={`paw-bg-primary ${styles.imageGallery}`}>
-                <div>
-                    <div > HHHHHA</div>
-                    <div><img src="/fashion-paw/public/media/second_pd/cat/cat2_home1_1.jpeg" alt="" /></div>
-                </div>
+const PdImageGallery = () => {
+    const images = [
+      '/media/second_pd/cat/cat2_home1_1.jpeg',
+      '/media/second_pd/cat/cat2_home1_2.jpeg',
+      '/media/second_pd/cat/cat2_home1_3.jpeg',
+      '/media/second_pd/cat/cat2_home1_4.jpeg',
+    ];
+  
+    //預設是放第一張圖
+/*     const stateArray = useState(images[0]);
+    const currentImage = stateArray[0];
+    const setCurrentImage = stateArray[1];
+ */
+    const [currentImage, setCurrentImage] = useState(images[0]); // 預設第一張
+  
+    return (
+        <>
+        <h1>這是商品圖片窗</h1>
+      <div className={`paw-bg-primary ${styles.imageGallery}`}>
+  
+        {/* 上方 */}
+        <div className={styles.main}>
+          <img src={currentImage} alt="展示圖" />
+        </div>
+  
+        {/* 下方小圖 */}
+        <div className={styles.small}>
+          {images.map((img, index) => (
+            <div
+              key={index}
+              className={styles.smallGallery}
+              onClick={() => setCurrentImage(img)}
+              style={{ cursor: 'pointer' }}
+            >
+              <img src={img} alt={`cat${index}`} />
             </div>
-            
-            </>
-        );
-    }
-}
- 
-export default PdImageGallery;
+          ))}
+        </div>
+      </div>
+      </>
+    );
+  };
+  
+  export default PdImageGallery;
+
