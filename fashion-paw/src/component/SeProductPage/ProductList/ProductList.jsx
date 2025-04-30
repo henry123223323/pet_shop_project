14f3af4cf271e64196a06b15d58780b673d0b5c8
a0812578 <a0812578@gmail.com>
2025-04-30 22:26:08 +0800

    productlist

--- fashion-paw/src/component/SeProductPage/ProductList/ProductList.jsx
+++ fashion-paw/src/component/SeProductPage/ProductList/ProductList.jsx
@@ -1,31 +1,4 @@
-import React from 'react';
-import ProductCard from '../ProductCard/ProductCard';
-import styles from './ProductList.module.css';
 
-export default function ProductList({
-  products = [],
-  favoriteIds = [],
-  onToggleFavorite,
-  onAddToCart,
-}) {
-  return (
-    <ul className={styles.list}>
-      {products.map(p => (
-        <li key={p.id}>
-          <ProductCard
-            id={p.id}
-            name={p.name}
-            price={p.price}
-            imageUrl={p.imageUrl}
-            isFavorite={favoriteIds.includes(p.id)}
-            onToggleFavorite={onToggleFavorite}
-            onAddToCart={onAddToCart}
-          />
-        </li>
-      ))}
-    </ul>
-  );
-}
 
 import React from 'react';
 import ProductCard from '../ProductCard/ProductCard';
