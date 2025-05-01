23a3e3d56e01cbfd9ede24b6b0c223f0c1103813
“jia-yiii” <chocotin1624@gmail.com>
2025-05-01 16:05:50 +0800

    修改路徑

--- fashion-paw/src/App.js
+++ fashion-paw/src/App.js
@@ -13,7 +13,7 @@ import Login from './component/MemberCenter/Login';
 import Register from './component/MemberCenter/Register';
 import Touch from './component/PetKnowledge/PartTouch/Touch';
 import DogGuide from './component/PetKnowledge/Novicefeeding/DogGuide';
-import SeProductPage from './component/SeproductPage/SeProductPage';
+import SeProductPage from './component/SeProductPage/SeProductPage';
 
 import Quiz from './component/PetKnowledge/PetQuiz/Quiz';
 import Header from './component/Homepage/Header';
