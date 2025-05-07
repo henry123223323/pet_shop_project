6566a64b182e58c900eaeeda9421fdc34b32f337
“jia-yiii” <chocotin1624@gmail.com>
2025-05-08 00:26:01 +0800

    完成新增訂單資料到後台

--- fashion-paw/src/App.js
+++ fashion-paw/src/App.js
@@ -9,25 +9,25 @@ import Footer from './component/Homepage/Footer';
 import Breadcrumbs from './component/share/BreadCrumbs';
 import Icon from './component/Homepage/Icon';
 
-import Homepage           from './component/Homepage/Homepage';
-import MemberCenter       from './component/MemberCenter/MyAccount';
-import Login              from './component/MemberCenter/Login';
-import Register           from './component/MemberCenter/Register';
-import ProductPage        from './component/ProductPage/ProductPage';
-import SeProductPage      from './component/SeProductPage/SeProductPage';
-import ProductDetailPage  from './component/ProductDetailPage/ProductDetailPage';
-import ShoppingCartPage   from './component/Cart/ShoppingCartPage';
-import CheckBillPage      from './component/CheckBill/CheckBillPage';
-import Helpme             from './component/Aboutus/helpme';
-import Needhelp           from './component/Aboutus/needhelp';
+import Homepage from './component/Homepage/Homepage';
+import MemberCenter from './component/MemberCenter/MyAccount';
+import Login from './component/MemberCenter/Login';
+import Register from './component/MemberCenter/Register';
+import ProductPage from './component/ProductPage/ProductPage';
+import SeProductPage from './component/SeProductPage/SeProductPage';
+import ProductDetailPage from './component/ProductDetailPage/ProductDetailPage';
+import ShoppingCartPage from './component/Cart/ShoppingCartPage';
+import CheckBillPage from './component/CheckBill/CheckBillPage';
+import Helpme from './component/Aboutus/helpme';
+import Needhelp from './component/Aboutus/needhelp';
 import PdDetailPageWrapper from './component/ProductDetailPage/PdDetailPageWrapper';
 
-import KnowledgeLayout    from './component/PetKnowledge/KnowledgeLayout';
-import ArticleDetail      from './component/PetKnowledge/ArticleDetail';
-import Touch              from './component/PetKnowledge/PartTouch/Touch';
-import Quiz               from './component/PetKnowledge/PetQuiz/Quiz';
+import KnowledgeLayout from './component/PetKnowledge/KnowledgeLayout';
+import ArticleDetail from './component/PetKnowledge/ArticleDetail';
+import Touch from './component/PetKnowledge/PartTouch/Touch';
+import Quiz from './component/PetKnowledge/PetQuiz/Quiz';
 
-import { CartProvider }   from './component/Cart/CartContext';
+import { CartProvider } from './component/Cart/CartContext';
 
 function App() {
   return (
@@ -37,17 +37,18 @@ function App() {
         <main className={styles.mainContent}>
           <Breadcrumbs />
           <Switch>
-            <Route path="/"                              component={Homepage} exact />
-            <Route path="/MemberCenter"                  component={MemberCenter} />
-            <Route path="/Login"                         component={Login} />
-            <Route path="/Register"                      component={Register} />
-            <Route path="/ProductPage"                   component={ProductPage} exact />
-            <Route path="/SeProductPage"                 component={SeProductPage} exact />
-            <Route path="/ProductDetailPage"             component={ProductDetailPage} exact />
-            <Route path="/ShoppingCartPage"              component={ShoppingCartPage} exact />
-            <Route path="/CheckBillPage"                 component={CheckBillPage} exact />
-            <Route path="/Aboutus"                       component={Helpme} exact />
-            <Route path="/Help"                          component={Needhelp} exact />
+            <Route path="/" component={Homepage} exact />
+            <Route path="/MemberCenter" component={MemberCenter} />
+            <Route path="/Login" component={Login} />
+            <Route path="/Register" component={Register} />
+            <Route path="/ProductPage" component={ProductPage} exact />
+            <Route path="/SeProductPage" component={SeProductPage} exact />
+            <Route path="/ProductDetailPage" component={ProductDetailPage} exact />
+            <Route path="/product/:pid" component={PdDetailPageWrapper} exact />
+            <Route path="/ShoppingCartPage" component={ShoppingCartPage} exact />
+            <Route path="/CheckBillPage" component={CheckBillPage} exact />
+            <Route path="/Aboutus" component={Helpme} exact />
+            <Route path="/Help" component={Needhelp} exact />
 
             {/* 寵物小知識：列表頁 */}
             <Route
@@ -76,7 +77,7 @@ function App() {
             {/* 其他 */}
             <Route path="/PetKnowledge" component={KnowledgeLayout} exact />
             <Route path="/PartTouch/Touch" component={Touch} exact />
-            <Route path="/PetQuiz/Quiz"    component={Quiz}  exact />
+            <Route path="/PetQuiz/Quiz" component={Quiz} exact />
           </Switch>
         </main>
         <Footer />
